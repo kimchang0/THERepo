5a6e1f84961d4bfa83c38435e9375eb5e0417c73
kimchang0 <newshfkfk@gmail.com>
2020-10-25 21:13:52 +0900

    수정

--- SpringPRJ/src/poly/controller/NewsController.java
+++ SpringPRJ/src/poly/controller/NewsController.java
@@ -1,5 +1,7 @@
 package poly.controller;
 
+import java.util.Iterator;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.stanford.nlp.pipeline.CoreSentence;
+import poly.dto.NewsDTO;
 import poly.service.INewsService;
+import poly.util.NLPUtil;
+
 
 @Controller
 public class NewsController {
@@ -19,18 +25,56 @@ public class NewsController {
 	private INewsService newsService;
 	
 	@RequestMapping(value="/Today/clawlingNews")
-	public String getMovieInfoFromWEB(HttpServletRequest request, HttpServletResponse response, ModelMap model) 
+	public String getNewsInfoFromWEB(HttpServletRequest request, HttpServletResponse response, ModelMap model) 
 	throws Exception {
 		
-		log.info(this.getClass().getName() + ".getMovieInfoFromWEB Start!");
+		log.info(this.getClass().getName() + ".getNewsInfoFromWEB Start!");
 		
 		int res = newsService.getNewsInfoFromWEB();
 		
 		model.addAttribute("res", String.valueOf(res));
 		
-		log.info(this.getClass().getName() + ".getMovieInfoFromWEB End!");
+		log.info(this.getClass().getName() + ".getNewsInfoFromWEB End!");
 		
 		return "/Today/clawlingNews";
 		
 	}
+	
+	@RequestMapping(value="/Today/viewNews")
+	public String getMovieInfoFromWEB(HttpServletRequest request, HttpServletResponse response, ModelMap model) 
+	throws Exception {
+		
+		log.info(this.getClass().getName() + ".getNewsInfoFromDB Start!");
+		
+		String news_no = "1";
+		
+		NewsDTO nDTO = new NewsDTO();
+		
+		nDTO.setNews_no(news_no);
+		
+		nDTO = newsService.getNewsInfoFromDB(nDTO);
+		
+		Iterator<CoreSentence> it = NLPUtil.sentence(nDTO.getNews_content());
+		
+		while(it.hasNext()) {
+			
+			CoreSentence sent = it.next();
+			
+			log.info(sent.text());
+			log.info(sent.tokens().get(0).originalText());
+			log.info(sent.tokens().get(0).index());
+			log.info(sent.lemmas());
+			
+		}
+		
+		log.info(this.getClass().getName() + ".getNewsInfoFromDB End!");
+		
+		return "/Today/viewNews";
+		
+	}
+	
+	
+	
+	
+	
 }

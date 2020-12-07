e6dfc605793fa1f57986729330b79fa3822b0949
kimchang0 <newshfkfk@gmail.com>
2020-12-08 00:28:25 +0900

    로그내용 수정

--- SpringPRJ/src/poly/controller/MainController.java
+++ SpringPRJ/src/poly/controller/MainController.java
@@ -181,7 +181,7 @@ public class MainController {
 		session.setAttribute("user_id", tDTO.getUser_id());
 		log.info(this.getClass().getName() + "sessionSet user_id : " + session.getAttribute("user_id"));
 		
-		log.info("TheService.TheSignUp 시작");
+		log.info(this.getClass().getName() + "TheService.TheSignUp 시작");
 		int res = userService.UserSignUp(tDTO);
 		log.info("TheService.TheSignUp 종료");
 		log.info("res : " + res);

c37f4f6959b1fa25ae5e5a79991e5e54aa309223
kimchang0 <newshfkfk@gmail.com>
2020-12-25 17:47:31 +0900

    로그내용 수정

--- SpringPRJ/src/poly/controller/MainController.java
+++ SpringPRJ/src/poly/controller/MainController.java
@@ -245,7 +245,7 @@ public class MainController {
         if(emailCheck!=null) res=1;
         
         log.info(this.getClass().getName() + "result : " + res);
-        log.info("if 함수 종료");
+        log.info(this.getClass().getName() + "if 함수 종료");
         
         log.info("emailCheck 종료");
         return res;
